c9ea3cb298efff0760ab24393e13bcad75b63c4b
Lance Page <6ftdotexe@users.noreply.github.com>
2020-08-09 20:18:12 -0500

    remove /build

---
+++ build/precache-manifest.c69d5d552fd18358cd598da116f572f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e7b3cbe7c3a426dfbf20ddedee5a8e",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "029274f5558e31bc9ba3",
+    "url": "/React-Portfolio/static/js/2.cf75d19c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfolio/static/js/2.cf75d19c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c87aae63b24de2592d0",
+    "url": "/React-Portfolio/static/js/main.e1ccd432.chunk.js"
+  },
+  {
+    "revision": "1dc5922d7385233dbc15",
+    "url": "/React-Portfolio/static/js/runtime-main.c2b22eab.js"
+  },
+  {
+    "revision": "2f34d0e91f0ce5efced00c3e7ec1f353",
+    "url": "/React-Portfolio/static/media/Resume.2f34d0e9.pdf"
+  }
+]);
